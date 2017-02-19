41dfa37d595d50b7c944240ad2c388379260512f
Zsolt Meszaros <zsolt@sowad.com>
2017-02-19 23:22:15 +0100

    Rename app.js to main.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ const extractCSS = new ExtractTextPlugin('./style.css');
 const DIST_DIR = path.resolve(__dirname, 'dist');
 const SRC_DIR = path.resolve(__dirname, 'src');
 const config = {
-  entry: `${SRC_DIR}/app/index.js`,
+  entry: {
+    'js/main.js': path.resolve(__dirname, 'src/app/index.js'),
   output: {
     path: `${DIST_DIR}/app`,
     filename: 'bundle.js',
